56fa65c5ffea30a7ec173459124aa37dd2841b37
Boris Timofeenko <boriswinner88@gmail.com>
2019-09-14 00:55:09 +1000

    init

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+    Дефолтный layout
+    <nuxt/>    
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.main {
+  background-color: black;
+  height: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="section home">
+    <div class="columns is-mobile">
+      <div class="column is-half"></div>
+      <div class="column is-half">
+        <div class="home__imageOfMe_container">
+          <img :src="imgSrc" class = "home__imageOfMe"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+// import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+  components: {
+    // Card
+  },
+  data () {
+    return {
+      imgSrc: require('@/assets/me.png')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.home {
+  background-color: black;
+
+  &__imageOfMe_container {
+    height: 50vh;
+  }
+
+  &__imageOfMe {
+    height: 100%;
+  }
+}
+</style>

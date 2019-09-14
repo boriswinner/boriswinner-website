d274619b6300b9fb7dd76b87149a2d9f1f73c8a2
Boris Timofeenko <boriswinner88@gmail.com>
2019-09-14 15:00:21 +1000

    minor fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <section class="section home">
     <div class="columns is-mobile">
-      <div class="column is-half">
+      <div class="column is-4">
         <div class="home__imageOfMe_container">
           <img :src="imgSrc" class = "home__imageOfMe">
         </div>
       </div>
-      <div class="column is-half">
+      <div class="column is-8">
         <p class="home__header"> Борис Тимофеенко </p>
+        <p class="home__paragraph"> Какой-то текст</p>
       </div>      
     </div>
   </section>
@@ -37,6 +38,13 @@ export default {
     font-family: 'Open Sans Condensed', sans-serif;   
     font-size: 48px;    
     color: white; 
+    font-weight: 300;
+  }
+
+  &__paragraph {
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 400;
+    color: #f2f2f2;
   }
 
   &__imageOfMe_container {
